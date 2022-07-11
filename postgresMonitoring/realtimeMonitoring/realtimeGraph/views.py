0710bf872ee74b7aed030d74cd041f12fda6141a
mgaitans <m.gaitans@uniandes.edu.co>
2022-07-11 18:43:03 -0500

    ajuste de funcion  de consulta

--- postgresMonitoring/realtimeMonitoring/realtimeGraph/views.py
+++ postgresMonitoring/realtimeMonitoring/realtimeGraph/views.py
@@ -768,6 +768,7 @@ def get_data_map_json(request, **kwargs):
     endFormatted = end.strftime("%d/%m/%Y") if end != None else " "
 
     data_result["locations"] = [loc.str() for loc in locations]
+    data_result["stations"]  = [loc.str() for loc in stations]
     data_result["start"] = startFormatted
     data_result["end"] = endFormatted
     data_result["data"] = data
