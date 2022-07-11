bda4cc44cb2d82a5c5e21e71d7891f5f8c3a9296
mgaitans <m.gaitans@uniandes.edu.co>
2022-07-11 15:31:34 -0500

    se agrega la ruta para la función previamente definida get_map_json_data en el archivo urls.py

--- timescaleMonitoring/realtimeMonitoring/realtimeGraph/urls.py
+++ timescaleMonitoring/realtimeMonitoring/realtimeGraph/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("historical/data", download_csv_data, name="historical-data"),
+    path("mapJsonData/", get_map_json_data, name="mapJsonData"),
 ]
